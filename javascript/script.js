c7ed871be5e5bf660aa520574ac246d200ca8cdb
Giovannyf99 <giovannyf1999@gmail.com>
2023-05-05 13:24:40 -0400

    bootstrap and fetch for javascript

--- javascript/script.js
+++ javascript/script.js
@@ -5,12 +5,24 @@ let recTime = document.querySelector('.recTime')
 let recIngred = document.querySelector('.recIngred')
 let recDirec = document.querySelector('.recDirec')
 
-    fetch("https://simplyvegan-backend-4h5s.onrender.com/recipes/random")
-        .then(response => response.json())
-        .then(data => {
-            console.log(data)
-          recTitle.innerText = data.name
-          recTime.innerText = data.time
-          recIngred.innerText = data.ingredients
-          recDirec.innerText = data.directions
-        });
+fetch("https://simplyvegan-backend-4h5s.onrender.com/recipes/random")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        recTitle.innerText = data.name
+        recTime.innerText = data.time
+        recIngred.innerText = data.ingredients
+        recDirec.innerText = data.directions
+    });
+
+
+const myVegans = document.getElementById('');
+let factsTitle = document.querySelector('.factsTitle')
+let factsTime = document.querySelector('.factsTime')
+
+fetch("https://simplyvegan-backend-4h5s.onrender.com/facts/random")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        factsTitle.innerText = data.fact
+    });
