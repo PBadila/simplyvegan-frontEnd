bf4ce58ff6ffedcf9fb851b086b301683a76f1d3
COR Coding <corcoding@CORs-MacBook-12.localdomain>
2023-05-08 12:20:15 -0400

    getting next and back buttons to show properly

--- javascript/script.js
+++ javascript/script.js
@@ -11,10 +11,13 @@ let recSearchTitle = document.querySelector('.recSearchTitle')
 let recSearchTime = document.querySelector('.recSearchTime')
 let recSearchIngred = document.querySelector('.recSearchIngred')
 let recSearchDirec = document.querySelector('.recSearchDirec')
-
+let nextButton = document.querySelector('.nextBtn')
+let backButton = document.querySelector('.backBtn')
 
 
 console.log(recSearchCardBody)
+
+// let searchBtn = 0   //counting search clicks
 
 
 // fetch for random recipe
@@ -27,39 +30,92 @@ fetch("https://simplyvegan-backend-4h5s.onrender.com/recipes/random")
         recIngred.innerText = data.ingredients
         recDirec.innerText = data.directions
     });
- console.log(searchAllRecBtn)
+console.log(searchAllRecBtn)
 
-  //search All Recipes Button 
-  searchAllRecBtn.addEventListener('click', ()=>{
-      let desiredingredient = searchIngredInput.value
-      fetch(`https://simplyvegan-backend-4h5s.onrender.com/recipes/${desiredingredient}`)
-      .then(response => response.json())
-      .then(data => {
-          console.log(data)
-          let recCount = 0
-          if(data.length > 0){
-            recSearchTitle.innerText = data[0].name
-            recSearchTime.innerText = data[0].time
-            recSearchIngred.innerText = data[0].ingredients
-            recSearchDirec.innerText = data[0].directions
+//search All Recipes Button 
+searchAllRecBtn.addEventListener('click', () => {
+    // searchBtn++
+    // console.log(searchBtn)
+    let desiredingredient = searchIngredInput.value
+    searchIngredInput.value = ""
+    fetch(`https://simplyvegan-backend-4h5s.onrender.com/recipes/${desiredingredient}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            let recCount = 0
+            if (data.length > 0) {
 
-            if(data.length >1){
-                //creates next button
-                let nextButton = document.createElement('button')
-                nextButton.classList.add('nextRecipe')
-                recSearchCardBody.appendChild(nextButton)
-                nextButton.innerText = "Next"
-                
-                //looping through the remaining recipes and creating card
-            
+                //filling out first recipe card
 
-                
+                recSearchTitle.innerText = data[0].name
+                recSearchTime.innerText = data[0].time
+                recSearchIngred.innerText = data[0].ingredients
+                recSearchDirec.innerText = data[0].directions
+
+                if (data.length > 1) {
+                    //creates next button
+                    console.log(recCount)
+                    console.log(data.length)
+                    // if (recCount < data.length-1) {
+                    nextButton.style.display = "block"
+                    // nextButton.classList.add('nextRecipe')
+                    // recSearchCardBody.appendChild(nextButton)
+                    // nextButton.innerText = "Next"
+                    // //  }
+                }
+                console.log(nextButton)
+                //if (nextButton.style.display == 'block') {
+
+
+                nextButton.addEventListener("click", e => {
+                    console.log('click')
+                    recCount++
+                    nextButton.style.display = 'none';
+
+                    console.log(recCount);
+                    recSearchTitle.innerText = data[recCount].name
+                    recSearchTime.innerText = data[recCount].time
+                    recSearchIngred.innerText = data[recCount].ingredients
+                    recSearchDirec.innerText = data[recCount].directions
+                    console.log(`recCount: ${recCount}`)
+                    console.log(`data.length: ${data.length}`)
+                    if (recCount >=1) {
+                    backButton.style.display = "block"
+                    }
+                    else {
+                        backButton.stlye.display = 'none'
+                    }
+                    if (data.length - 1 > recCount) {
+                        // let nextButton = document.createElement('button')
+                        // nextButton.classList.add('nextRecipe')
+                        // recSearchCardBody.appendChild(nextButton)
+                        // nextButton.innerText = "Next"
+                        nextButton.style.display = 'block'
+                        console.log(`${data.length - 1 > recCount}`)
+                    }
+                    else {
+                        nextButton.style.display = 'none'
+                        console.log(`${data.length - 1 > recCount}`)
+                    }
+
+                })
+                backButton.addEventListener('click', e => {
+                    if (recCount >=1 ) {
+                    recSearchTitle.innerText = data[recCount -1].name
+                    recSearchTime.innerText = data[recCount -1].time
+                    recSearchIngred.innerText = data[recCount -1].ingredients
+                    recSearchDirec.innerText = data[recCount -1].directions
+                    recCount --
+                    console.log(`recCount${recCount}`)
+                    }
+
+                })
+                //}
+
             }
-          }
+        });
+});
 
-      });
-  });
-   
 
 
 
@@ -71,18 +127,18 @@ let refreshBtn = document.querySelector(".btn-primary")
 
 // loads random fact on page load
 fetch("https://simplyvegan-backend-4h5s.onrender.com/facts/random")
-   .then(response => response.json())
-   .then(data => {
-       console.log(data)
-       factsTitle.innerText = data.fact
-   });
-
-// refreshes random fact on button click
-refreshBtn.addEventListener("click", () => {
-    fetch("https://simplyvegan-backend-4h5s.onrender.com/facts/random")
     .then(response => response.json())
     .then(data => {
         console.log(data)
         factsTitle.innerText = data.fact
     });
-    }); 
+
+// refreshes random fact on button click
+refreshBtn.addEventListener("click", () => {
+    fetch("https://simplyvegan-backend-4h5s.onrender.com/facts/random")
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            factsTitle.innerText = data.fact
+        });
+}); 
